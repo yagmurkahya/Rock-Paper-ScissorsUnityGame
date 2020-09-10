46b8bf7f503895b3930bc9dc4164c0c9eec5fe4e
yagmurkahya <70861513+yagmurkahya@users.noreply.github.com>
2020-09-10 15:21:47 +0300

    sc

---
+++ source codes/panelbuttons.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class panelbuttons : MonoBehaviour
+{
+
+public GameObject PausePanel, PauseButton, QuitButton;
+
+    public void Pause(){
+        PausePanel.SetActive(true);
+        PauseButton.SetActive(false);
+    }
+
+ public void Resume(){
+     PausePanel.SetActive(false);
+     PauseButton.SetActive(true);
+     
+    }
+public void Quitbutton(){
+        Debug.Log ("Quit Game..");
+        Application.Quit();
+
+}
+}
+    
---
+++ source codes/scenegecis.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scenegecis : MonoBehaviour
+{
+    public void sahnedegis(int sceneid){
+        SceneManager.LoadScene(sceneid);
+    }
+}
